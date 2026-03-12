4f10049451a36201ca969b38ae87dc59b4434a3f
Jan Stenner <jan.stenner@upb.de>
2026-03-12 15:08:18 +0100

    positional_encoding_decoder for agent_mat

--- src/agent_mat.jl
+++ src/agent_mat.jl
@@ -410,7 +410,7 @@ end
 (pe::SinCosPositionEmbed)(idxs::AbstractVector{<:Integer}) = @view pe.pe[:, idxs]
 
 
-function create_agent_mat(;action_space, state_space, use_gpu, rng, y, p, update_freq = 256, nna_scale = 1, nna_scale_critic = nothing, network_depth = 2, network_depth_critic = nothing, drop_middle_layer = nothing, drop_middle_layer_critic = nothing, learning_rate = 0.00001, fun = leakyrelu, fun_critic = nothing, n_actors = 1, clip1 = false, n_epochs = 4, n_microbatches = 4, normalize_advantage = true, logσ_is_network = false, start_steps = -1, start_policy = nothing, max_σ = 2.0f0, actor_loss_weight = 1.0f0, critic_loss_weight = 0.5f0, entropy_loss_weight = 0.00f0, adaptive_weights = false, clip_grad = 0.5, target_kl = 100.0, start_logσ = 0.0, dim_model = 64, block_num = 1, head_num = 4, head_dim = nothing, ffn_dim = 120, drop_out = 0.1, betas = (0.99, 0.99), jointPPO = false, customCrossAttention = true, one_by_one_training = false, clip_range = 0.2f0, tanh_end = false, positional_encoding = 1, useSeparateValueChain = false, verbose = false)
+function create_agent_mat(;action_space, state_space, use_gpu, rng, y, p, update_freq = 256, nna_scale = 1, nna_scale_critic = nothing, network_depth = 2, network_depth_critic = nothing, drop_middle_layer = nothing, drop_middle_layer_critic = nothing, learning_rate = 0.00001, fun = leakyrelu, fun_critic = nothing, n_actors = 1, clip1 = false, n_epochs = 4, n_microbatches = 4, normalize_advantage = true, logσ_is_network = false, start_steps = -1, start_policy = nothing, max_σ = 2.0f0, actor_loss_weight = 1.0f0, critic_loss_weight = 0.5f0, entropy_loss_weight = 0.00f0, adaptive_weights = false, clip_grad = 0.5, target_kl = 100.0, start_logσ = 0.0, dim_model = 64, block_num = 1, head_num = 4, head_dim = nothing, ffn_dim = 120, drop_out = 0.1, betas = (0.99, 0.99), jointPPO = false, customCrossAttention = true, one_by_one_training = false, clip_range = 0.2f0, tanh_end = false, positional_encoding = 1, positional_encoding_decoder = nothing, useSeparateValueChain = false, verbose = false)
 
     isnothing(nna_scale_critic)         &&  (nna_scale_critic = nna_scale)
     !isnothing(drop_middle_layer)        &&  (network_depth = drop_middle_layer ? 1 : 2)
@@ -420,6 +420,7 @@ function create_agent_mat(;action_space, state_space, use_gpu, rng, y, p, update
     network_depth_critic = max(1, Int(network_depth_critic))
     isnothing(fun_critic)               &&  (fun_critic = fun)
     isnothing(head_dim)                 &&  (head_dim = Int(floor(dim_model/head_num)))
+    isnothing(positional_encoding_decoder) && (positional_encoding_decoder = positional_encoding)
 
     init = Flux.glorot_uniform(rng)
 
@@ -474,12 +475,17 @@ function create_agent_mat(;action_space, state_space, use_gpu, rng, y, p, update
 
     if positional_encoding == 1
         position_encoding_encoder = SinCosPositionEmbed(dim_model)
-        position_encoding_decoder = SinCosPositionEmbed(dim_model)
     elseif positional_encoding == 2
         position_encoding_encoder = Embedding(context_size => dim_model)
-        position_encoding_decoder = Embedding(context_size => dim_model)
     elseif positional_encoding == 3
         position_encoding_encoder = ZeroEncoding(dim_model)
+    end
+
+    if positional_encoding_decoder == 1
+        position_encoding_decoder = SinCosPositionEmbed(dim_model)
+    elseif positional_encoding_decoder == 2
+        position_encoding_decoder = Embedding(context_size => dim_model)
+    elseif positional_encoding_decoder == 3
         position_encoding_decoder = ZeroEncoding(dim_model)
     end
 
